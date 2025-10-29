b6e0581cefeb1e0dcdea22f3c942a4928ee74ac1
Md-Gajjali <hellogajjali@gmail.com>
2025-10-29 15:11:02 +0600

    thirdbtn update

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ secondBtn.addEventListener('click', () => {
     secondError.innerHTML = 'please enter a number 😒'
   } else if (! secondValue) {
     secondError.innerHTML = 'only number are allowed'
-  }else if (secondInput.value > 0 && secondInput.value >= 10) {
+  }else if (secondInput.value > 10 || secondInput.value < 0  ) {
     secondError.innerHTML='please inter a number between 1 to 10'
   }else {
     secondUi.style.display = 'none'
@@ -77,58 +77,3 @@ ThirdBtn.addEventListener('click', () => {
 })
 
 
-
-
-
-
-
-
-
-window.onload = () => {
-    let button = document.querySelector("#btn");
-
-    // Function for calculating BMI
-    button.addEventListener("click", calculateBMI);
-};
-
-function calculateBMI() {
-
-    /* Getting input from user into height variable.
-    Input is string so typecasting is necessary. */
-    let height = parseInt(document
-        .querySelector("#height").value);
-
-    /* Getting input from user into weight variable. 
-    Input is string so typecasting is necessary.*/
-    let weight = parseInt(document
-        .querySelector("#weight").value);
-
-    let result = document.querySelector("#result");
-
-    // Checking the user providing a proper
-    // value or not
-    if (height === "" || isNaN(height))
-        result.innerHTML = "Provide a valid Height!";
-
-    else if (weight === "" || isNaN(weight))
-        result.innerHTML = "Provide a valid Weight!";
-
-    // If both input is valid, calculate the bmi
-    else {
-
-        // Fixing upto 2 decimal places
-        let bmi = (weight / ((height * height)
-            / 10000)).toFixed(2);
-
-        // Dividing as per the bmi conditions
-        if (bmi < 18.6) result.innerHTML =
-            `Under Weight : <span>${bmi}</span>`;
-
-        else if (bmi >= 18.6 && bmi < 24.9)
-            result.innerHTML =
-                `Normal : <span>${bmi}</span>`;
-
-        else result.innerHTML =
-            `Over Weight : <span>${bmi}</span>`;
-    }
-}
